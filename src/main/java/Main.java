368c9a99cca65eaf9e1ae52dc9e0753474b8b826
Radu599 <30553913+Radu599@users.noreply.github.com>
2019-12-05 09:38:01 +0200

    commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.net.URL;
+
+public class Main {
+    private static final String versionHash = "ec60501505";
+    public static void main(String[] args) {
+        System.out.println("NEW " + versionHash);
+    }
+
+}
