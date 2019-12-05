4d4aa2a8ce009be356064db01050cb583c1c5739
Radu599 <30553913+Radu599@users.noreply.github.com>
2019-12-05 10:13:36 +0200

    new

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ public class Main {
     private static final String versionHash = "368c9a99cc";
     public static void main(String[] args) {
         System.out.println("NEW " + versionHash);
+
+
     }
 
 }
